44f0d204eacf166808bb14bae486d8871469148b
jin <jin@localhost.localdomain>
2017-04-24 22:07:50 +0800

    1.目前可以进行滑动操作，touch.py 里面是关于手机滑动的，通过sendevent实现

---
+++ cf/test/test3.py
@@ -0,0 +1,14 @@
+
+from cf.touch import startTouch
+from cf.touch import moveTouch
+from cf.touch import endTouch
+
+
+def main() :
+    startTouch(10, 10)
+    for num in range(102):
+        moveTouch(20, num *10)
+    endTouch()
+
+if __name__ == '__main__':
+    main()
--- cf/touch.py
+++ cf/touch.py
@@ -1,3 +1,4 @@
+
 import os
 def main() :
 
@@ -5,15 +6,28 @@ def main() :
 
 def startTouch(x, y):
 
-    os.system("adb shell sendevent /dev/input/event4 3 58 62") # ABS_MT_PRESSURE   ===开始
+    os.system("adb shell sendevent /dev/input/event4 3 58 62") # ABS_MT_PRESSURE   ===
     os.system("adb shell sendevent /dev/input/event4 3 53 " + str(x)) # ABS_MT_POSITION_X
     os.system("adb shell sendevent /dev/input/event4 3 54 "+ str(y)) # ABS_MT_POSITION_Y
     os.system("adb shell sendevent /dev/input/event4 3 57 0") # ABS_MT_TRACKING_ID
-    os.system("adb shell sendevent /dev/input/event4 0 2 0") # SYN_MT_REPORT    === 用于同步分离
+    os.system("adb shell sendevent /dev/input/event4 0 2 0") # SYN_MT_REPORT    ===
     os.system("adb shell sendevent /dev/input/event4 1 330 1") # BTN_TOUCH            DOWN
 
-def move(x,y):
-    os.system()
+def moveTouch(x,y):
+
+    os.system("adb shell sendevent /dev/input/event4 3 58 40")  # ABS_MT_PRESSURE   ===
+    os.system("adb shell sendevent /dev/input/event4 3 53 " + str(x))  # ABS_MT_POSITION_X
+    os.system("adb shell sendevent /dev/input/event4 3 54 " + str(y))  # ABS_MT_POSITION_Y
+    os.system("adb shell sendevent /dev/input/event4 3 57 0")  # ABS_MT_TRACKING_ID
+    os.system("adb shell sendevent /dev/input/event4 0 2 0")  # SYN_MT_REPORT    ===
+    os.system("adb shell sendevent /dev/input/event4 0 0 0")  # BTN_TOUCH            DOWN
+
+
+def endTouch():
+    os.system("adb shell sendevent /dev/input/event4 0 2 0") # SYN_MT_REPORT
+    os.system("adb shell sendevent /dev/input/event4 1 330 0") # BTN_TOUCH
+    os.system("adb shell sendevent /dev/input/event4 0 0 0") #SYN_REPORT
+
 
 if __name__ == '__main__':
     main()
